a3b1361f8ddb8c38f05a3bc8a1b2bcd62d1e5408
luucasor <lucas@jornadatecnologia.com>
2023-07-27 15:41:30 -0300

    Arranging the calculator buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,16 @@
 import './Button.css'
 
-export default props =>
-    <button className='button'>{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : '' 
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    return (
+        <button 
+            className={classes}
+            onClick={e => props.click && props.click(props.label)}s
+        >
+            {props.label}
+        </button>
+    )
+}
